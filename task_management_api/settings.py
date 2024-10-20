94f5e41f9ab8e0c68147dc40a89560e99d7e5cbc
Nokuphila Mkhize <163729384+Nokuphila11@users.noreply.github.com>
2024-10-20 12:46:35 +0200

    settings.py

--- task_management_api/settings.py
+++ task_management_api/settings.py
@@ -17,8 +17,6 @@ import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -93,7 +91,6 @@ DATABASES = {
         'default': dj_database_url.config(default='postgres://localhost'),
         
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Use os.path.join()
     }
 }
